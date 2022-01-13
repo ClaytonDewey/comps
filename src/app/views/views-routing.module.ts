9a9696e17c9f63bcae2005eb8e67990902ae755b
Clay <clay@dryadmedia.com>
2022-01-13 15:35:55 -0600

    Display list of statistics

--- src/app/views/views-routing.module.ts
+++ src/app/views/views-routing.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewsHomeComponent } from './views-home/views-home.component';
-import { StatisticsComponent } from './statistics/statistics.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: ViewsHomeComponent,
-        children: [
-            { path: './', component: StatisticsComponent }
-        ]
-    }
+    { path: '', component: ViewsHomeComponent }
 ];
 
 @NgModule({
